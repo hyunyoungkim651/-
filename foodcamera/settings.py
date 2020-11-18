0a0d1c23b3c00927f1a8040fed7d9bd46a185f6a
unknown <k00810111@gmail.com>
2020-11-18 16:57:07 +0900

    2020-11-18, signup

--- foodcamera/settings.py
+++ foodcamera/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'foodcamera.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [ BASE_DIR / 'subdir', ],
+        'DIRS': [ BASE_DIR / 'templates', ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- partner/views.py
+++ partner/views.py
@@ -1,3 +1,18 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    ctx = {}
+    return render(request, "index.html", ctx)
+
+def signup(request):
+    if request.method == "GET":
+        pass
+    elif request.method == "POST":
+        username = request.POST.get("username")
+        content = request.POST.get("content")
+
+
+    ctx = {}
+    return render(request, "signup.html", ctx)
